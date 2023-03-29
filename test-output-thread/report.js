fc9c36f9c7ee8e92d3d6fc94e989edfb501fc227
madhusudhan <madhusudhan@upstreamgs.com>
2023-03-29 11:34:15 +0530

    first commit

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"25be99f6-76ea-43c4-a737-e09473e2f8ea","feature":"Search product and select and add to cart with other functionalities","scenario":"Adding product to the cart and validating accordingly","start":1680068636732,"group":1,"content":"","tags":"","end":1680068865830,"className":"passed"},{"id":"c344259a-2028-4f63-abfa-097e01bc76c3","feature":"Login Page Feature","scenario":"Customer login to the app and validate Forgot Password Link","start":1680068865852,"group":1,"content":"","tags":"","end":1680068882242,"className":"failed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
